d27c233571566b3b210aadef3421f90286c1c0b5
e6nlaq <120777733+e6nlaq@users.noreply.github.com>
2023-04-05 20:42:43 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# chikusho
+
+[コウメ太夫](https://twitter.com/dayukoume)さんの[#まいにちチクショー](https://twitter.com/hashtag/%E3%81%BE%E3%81%84%E3%81%AB%E3%81%A1%E3%83%81%E3%82%AF%E3%82%B7%E3%83%A7%E3%83%BC?src=hashtag_click)風のをランダムに生成します。
---
+++ index.js
@@ -0,0 +1,51 @@
+
+function rand(min, max) {
+	return Math.random() * (max - min) + min;
+}
+
+function arra(arr) {
+	return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function sharp(str) {
+	let stra = "";
+	for (let i = 0; i < str.length; i++) {
+		if (str[i] == "#") {
+			stra += "%23";
+		} else {
+			stra += str[i];
+		}
+	}
+
+	return stra;
+}
+
+function gen() {
+	const one = ["ラーメン", "アイスクリーム", "運転", "お月様", "ゲーム", "エイプリールフール", "富士山", "Misskey", "イーロンマスク", "AI絵師", "再共有", "バグ", "改悪", "ゴッド", "吐き気", "石鹸", "スガキヤ", "埼玉"];
+	const two = ["を食べてた", "してた", "を見てた", "を壊した", "を舐めた", "がキモかった", "かと思った", "を感じた", "を見つけた", "がした"];
+	const thr = ["臆病", "味付き", "コンセント", "index", "ふすま", "群馬帝国", "最悪", "山梨", "さかな", "青だぬき", "偽物", "本物", "Google", "マジもん"];
+
+	const ans = `${arra(one)}${arra(two)}ら～、\n\n${arra(thr)}でした～。\n\nチクショー！！　#いつでもチクショー`;
+
+	// alert(ans);
+	document.getElementById('ret').textContent = ans;
+}
+
+function tweet() {
+	let text = document.getElementById('ret');
+	text = encodeURI(text.value);
+	text = sharp(text);
+	window.location.href = `https://twitter.com/intent/tweet?text=${text}`;
+}
+
+function copy() {
+	let text = document.getElementById('ret');
+	navigator.clipboard.writeText(text.value).then(
+		() => {
+			alert("コピーしました!");
+		},
+		() => {
+			alert("コピーに失敗しました");
+		}
+	);
+}
